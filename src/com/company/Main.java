44a9d4cbecbea2a5a22c37b9d00414cebeab4a56
Catalin Trandafir <cata627@gmail.com>
2018-03-16 20:09:17 +0100

    Ordenaiko

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,13 @@
 package com.company;
 
+import com.company.view.screens.Acceso;
 import com.company.view.screens.MenuUsuario;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        new MenuUsuario().start();
+        new Acceso().start();
     }
 }
 //Creando Rama
--- src/com/company/view/screens/Acceso.java
+++ src/com/company/view/screens/Acceso.java
@@ -5,8 +5,19 @@ import java.util.Scanner;
 public class Acceso {
     char opcion;
 
-    void start(){
+    public void start(){
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Que quieres hacer?");
+        System.out.println("a. Acceder.\nb. Registrarte.");
+        opcion = scanner.nextLine().charAt(1);
+        switch (opcion){
+            case 'a':
+
+                break;
+            case 'b':
+                Registro.start();
+                break;
+        }
 
     }
 }
