49a8919a4fcf7ab4e1063b72f5ff958d9322d6ae
Catalin Trandafir <cata627@gmail.com>
2018-03-20 20:04:14 +0100

    Ordenaiko

--- src/com/company/view/screens/Acceder.java
+++ src/com/company/view/screens/Acceder.java
@@ -1,4 +1,24 @@
 package com.company.view.screens;
 
+import com.company.manager.ManagerUsuario;
+import com.company.model.Usuario;
+import com.company.view.widget.EditText;
+
 public class Acceder {
+
+        public void start(){
+            boolean noValido;
+            do {
+
+                ManagerUsuario managerUsuario = new ManagerUsuario();
+                String usuario = new EditText("Usuario: ").pedirString();
+                String contraseña = new EditText("Contraseña: ").pedirString();
+
+                noValido = managerUsuario.verificarUsuario(usuario, contraseña);
+                if(noValido){
+                    System.out.println("Usuario/Contraseña incorrectos.");
+                }
+            }while(noValido);
+
+        }
 }
