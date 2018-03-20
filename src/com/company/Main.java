d32bfed5b6c1ee53b2f896777c4ece4e28e6616d
Ismael Tejada <ismael.elpuig@gmail.com>
2018-03-20 20:05:43 +0100

    Re-master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import com.company.manager.ManagerUsuario;
 import com.company.view.screens.Acceso;
 import com.company.view.screens.MenuUsuario;
 import com.company.view.widget.EditText;
@@ -10,4 +11,5 @@ public class Main {
     public static void main(String[] args) {
         new Acceso().start();
     }
+    ManagerUsuario managerUsuario = new ManagerUsuario();
 }
