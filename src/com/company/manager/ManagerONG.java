643dce293bd023bffe69f66ed88a15cbbffcbad8
Ismael Tejada <ismael.elpuig@gmail.com>
2018-04-16 19:22:30 +0200

    Re-master

--- src/com/company/manager/ManagerONG.java
+++ src/com/company/manager/ManagerONG.java
@@ -1,6 +1,7 @@
 package com.company.manager;
 
 import com.company.model.ONG;
+import com.company.model.Usuario;
 
 public class ManagerONG {
     public ONG[] ONGs = new ONG[100];
@@ -50,4 +51,27 @@ public class ManagerONG {
         }
         return nombres;
     }
+
+
+    public ONG encontrarONG(String encontrarONG){
+
+        for (int i = 0; i <ONGs.length ; i++) {
+            if (ONGs[i]!=null && ONGs[i].nombre.equals(encontrarONG)){
+                return ONGs[i];
+            }
+        }
+        return null;
+    }
+
+    public void cambiarNombreONG(ONG ong, String nuevoNombreONG){
+        ong.nombre=nuevoNombreONG;
+    }
+
+
+    public void cambiarNombrePaisONG(ONG ong, String nuevoNombrePaisONG){
+        ong.nombre=nuevoNombrePaisONG;
+    }
+
+
+
 }
--- src/com/company/view/screens/GestionarONGs.java
+++ src/com/company/view/screens/GestionarONGs.java
@@ -1,4 +1,51 @@
 package com.company.view.screens;
 
+import com.company.manager.ManagerONG;
+import com.company.manager.ManagerUsuario;
+import com.company.view.widget.EditText;
+import com.company.view.widget.Menu;
+import com.company.view.widget.TextoColor;
+
 public class GestionarONGs {
+
+    Menu menu = new Menu(3,1);
+
+
+    public void start(ManagerUsuario managerUsuario,ManagerONG managerONG){
+
+        menu.showMenuUsuario("Panel de control de ONGs",managerUsuario,managerONG,"Cambiar nombre", "Cambiar Pais","Volver al menú");
+
+        switch (menu.option()) {
+            case 1:
+                try {
+                    managerONG.cambiarNombreONG(managerONG.encontrarONG(new EditText("Indique el nombre de la ONG").pedirString()),new EditText("Indique el nuevo nombre").pedirString());
+                    new TextoColor().colorCheck("Username cambiado con éxito");
+                }catch (Exception e){
+                    new TextoColor().colorError("No existe una ONG con ese nombre");
+                }
+                new GestionarUsuarios().start(managerUsuario,managerONG);
+
+                break;
+
+            case 2:
+                try {
+                    managerONG.cambiarNombrePaisONG(managerONG.encontrarONG(new EditText("Indique el pais de la ONG a modificar").pedirString()),new EditText("Indique el nuevo pais").pedirString());
+                    new TextoColor().colorCheck("Username cambiado con éxito");
+                }catch (Exception e){
+                    new TextoColor().colorError("No existe esa ONG");
+                }
+                new GestionarUsuarios().start(managerUsuario, managerONG);
+
+                break;
+
+
+
+            case 3:
+                new MenuUsuario().startMenuUsuario(managerUsuario, managerONG);
+
+            default:
+                break;
+        }
+    }
+
 }
